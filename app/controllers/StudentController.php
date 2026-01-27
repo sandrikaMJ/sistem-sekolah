05eb239a63f8f4a6f67246b87bd342fa5068bbbb
sandrikaMJ <sandrika810@gmail.com>
2026-01-27 08:34:33 +0700

    feat: implement student controller

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controllers;
+
+class StudentControllers
+{
+    public function index()
+    {
+            echo '<h1>Daftar Siswa</h1>';
+            echo '<p>Menampilkan daftar siswa</p>';
+    }
+
+
+    public function create()
+    {
+        
+    }
+
+
+}
+
+?>
--- app/core/Router.php
+++ app/core/Router.php
@@ -1,24 +1,35 @@
 <?php
-
 namespace App\Core;
-
+ 
+use App\Controllers\StudentControllers;
+ 
 class Router
 {
-
+ 
     public function run()
     {
-        $method = $_SERVER['REQUEST_METHOD'];
-        $uri= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-        if($method == 'GET' && $uri == '/students'){
-            echo '<h1>Daftar Siswa</h1>';
-            echo '<p>Menampilkan daftar siswa</p>';
+       $method = $_SERVER['REQUEST_METHOD'];
+       $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+ 
+        if($method == 'GET' && $uri == '/students') {
+            require_once './app/controllers/StudentController.php';
+            $controller = new StudentControllers();
+            $controller->index();
             return;
         }
-
+ 
+        if($method == 'GET' && $uri == '/students/create') {
+            require_once './app/controllers/StudentController.php';
+            $controller = new StudentControllers();
+            $controller->create();
+            return;
+        }
+ 
         http_response_code(404);
         echo '<h1>404 - Page Not Found</h1>';
+ 
     }
-
+ 
+   
 }
 ?>
