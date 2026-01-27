72239bd3426c6c2092f8fea99656b78d660eef83
sandrikaMJ <sandrika810@gmail.com>
2026-01-27 08:05:45 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -5,10 +5,20 @@ namespace App\Core;
 class Router
 {
 
-public function run()
-{
-echo 'Router is running...';
-}
-}
+    public function run()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+        if($method == 'GET' && $uri == '/students'){
+            echo '<h1>Daftar Siswa</h1>';
+            echo '<p>Menampilkan daftar siswa</p>';
+            return;
+        }
+
+        http_response_code(404);
+        echo '<h1>404 - Page Not Found</h1>';
+    }
+
+}
 ?>
