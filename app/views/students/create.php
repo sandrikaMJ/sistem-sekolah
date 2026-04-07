a3696427c7dc7fe89e54037226c23967c502c13b
sandrikaMJ <sandrika810@gmail.com>
2026-04-07 07:29:24 +0700

    feat: implementt submit button

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -46,6 +46,12 @@
                         <label class="block font-bold" for="phone_number">No Telepon</label>
                         <input class="w-full border rounded-lg py-2 px-4" type="text" name="phone_number" id="phone_number" placeholder="Masukkan No Telepon">
                     </div>
+
+                    <div class="flex justify-end gap-4 col-span-2">
+                        <a href="/students" class="py-2 px-4 bg-gray-100 rounded-lg">Kembali</a>
+                        <button type="submit" class="py-2 px-4 bg-blue-500 rounded-lg text-white">Simpan</button>
+                    </div>
+                
                 </form>
             </div>
 
